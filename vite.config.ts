2ffdd0d0137001657afe5b48e52dba28f6d176d7
KarmenZhan - M Moser Associates <karmenz@HKGNB-NOTE9-51.intranet.mmoser.com>
2026-02-02 14:06:46 +0800

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Strategy-Tool/',
   plugins: [react()],
   define: {
-    'process.env': {} 
+    'process.env': {}
   }
 });
+
